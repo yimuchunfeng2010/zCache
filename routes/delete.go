31f8c0200a2422ed7884e9bac369c58dde998e60
zengyuanjun <zengyuanjun@g7.com.cn>
2018-12-11 13:48:07 +0800

    fix:修复DELETE接口bug

--- routes/delete.go
+++ routes/delete.go
@@ -16,11 +16,12 @@ func Delete(context *gin.Context){
 		return
 	}
 
-	_ , err := Data.Delete(global.GlobalVar.Root, key)
+	node , err := Data.Delete(global.GlobalVar.Root, key)
 	if err != nil {
 		context.JSON(http.StatusNotFound,gin.H{"value":"","status":"done"})
 		return
 	} else {
+		global.GlobalVar.Root = node
 		context.JSON(http.StatusOK,gin.H{"key":key, "status":"done"})
 		return
 	}

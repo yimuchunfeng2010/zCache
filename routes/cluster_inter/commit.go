bb0e96f923ef027620c31fe4bbd84b22ad5010af
zengyuanjun <zengyuanjun@g7.com.cn>
2018-12-27 19:55:58 +0800

    commit函数部分功能

--- routes/cluster_inter/commit.go
+++ routes/cluster_inter/commit.go
@@ -2,13 +2,38 @@ package cluster_inter
 
 import (
 	"ZCache/tool"
+	"ZCache/global"
 	"ZCache/tool/logrus"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func Commit(context *gin.Context) {
-	commitID := context.Param("commitID")
+	commitID ,_ := strconv.ParseInt(context.Param("commitID"), 10, 64)
 	logrus.Infof("%s commitID Key:%s\n", tool.GetFileNameLine(), commitID)
+
+	global.GlobalVar.GInternalLock.Lock()
+	defer global.GlobalVar.GInternalLock.Unlock()
+	curNode := global.GlobalVar.GPreDoReqList
+	preNode := global.GlobalVar.GPreDoReqList
+	if curNode != nil{
+		if curNode.CommitID == commitID {
+			//TODO
+			return
+		}else{
+			curNode = curNode.Next
+		}
+	}
+
+	for curNode != nil {
+		if curNode.CommitID == commitID{
+			preNode.Next = curNode.Next
+			// TODO
+			return
+		}
+		preNode = curNode
+		curNode = curNode.Next
+	}
 
 	//if err != nil {
 	//	context.JSON(http.StatusInternalServerError, gin.H{"status": "NoACK", "reason": err.Error()})
